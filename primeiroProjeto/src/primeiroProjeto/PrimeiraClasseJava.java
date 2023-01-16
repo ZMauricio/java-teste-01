f04d0812dc2d2ec0d8511db837a82438fa328d7a
Mauricio <Mauricio@DESKTOP-K6KN4HB>
2023-01-16 17:14:41 -0300

    commit 02

--- primeiroProjeto/src/primeiroProjeto/PrimeiraClasseJava.java
+++ primeiroProjeto/src/primeiroProjeto/PrimeiraClasseJava.java
@@ -1,11 +1,5 @@
 package primeiroProjeto;
 
 public class PrimeiraClasseJava {
-	
-	/* Primeiro programa em Java */
-	public static void main(String[] args) {
-		
-		System.out.println("Olá Mundo!");
-	}
 
 }
